24c15c94bfabe5dde4ec7510404c00c806675967
Rustifer1000 <RUSSELLCOLLINSJR@GMAIL.COM>
2024-12-03 17:57:59 -0800

    Update model_config.py

--- FilesApp/src/constants/model_config.py
+++ FilesApp/src/constants/model_config.py
@@ -19,3 +19,4 @@ MODEL_CONFIGS = {
     "gpt-4-turbo-preview": ModelConfig("gpt-4-turbo-preview", 30, 0.0015, 16385, 500, 0.85),
     "gpt-4o": ModelConfig("gpt-4o", 12, 0.03, 3072, 2000, 1.0),
     "text-embedding-3-large": ModelConfig("text-embedding-3-large", 100, 0.0004, 3072, 100, 0.90)
+}
